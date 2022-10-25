10f6d0bdce7e444787ec2e607b42f08fe0f77ed1
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-10-24 22:49:10 -0700

    use

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -74,19 +74,19 @@ const UserContextProvider = ({children}) => {
         usersDispatch({type: 'DECREMENT', withdraw: withdraw});
     }
 
-    useEffect(() => {
-        localStorage.setItem("currentUser", JSON.stringify(users.currentUser)) 
+    // useEffect(() => {
+    //     localStorage.setItem("currentUser", JSON.stringify(users.currentUser)) 
         
-        if(users){
-            let result = users?.allUsers?.findIndex(x => {
-                return x.email === users?.currentUser?.email;
-            })
+    //     if(users){
+    //         let result = users?.allUsers?.findIndex(x => {
+    //             return x.email === users?.currentUser?.email;
+    //         })
 
-            users.allUsers[result].deposit = users.currentUser.deposit;
-            localStorage.setItem("allUser", JSON.stringify(users.allUsers));
-        }
-        return
-    },[users])
+    //         users.allUsers[result].deposit = users.currentUser.deposit;
+    //         localStorage.setItem("allUser", JSON.stringify(users.allUsers));
+    //     }
+    //     return
+    // },[users])
 
     return (
         <UserContext.Provider value={{register, login, logout, users, handleDeposit, handleWithdraw}}>
