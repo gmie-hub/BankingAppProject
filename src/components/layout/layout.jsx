ff729f84b3bf50648324095dc89d45c6f1cafa56
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-10-25 13:15:57 -0700

    nav

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Col, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { authRoutes, routes } from "../../routes";
+import { authRoutes} from "../../routes";
+import CustomNav from "../nav";
 
 const Layout =({children})=>{
     if (authRoutes.indexOf(window.location.pathname) !== -1){
@@ -9,20 +8,8 @@ const Layout =({children})=>{
     }
     return( 
        <React.Fragment>
-         <Row className="m-0">
-            <Row  className="spa " style={{background: "rgba(134, 69, 193, 0.726158)"}} >
-            <Stack direction="horizontal">
-                <div>
-                    <Link className="deco" to={routes.Welcome}>Home</Link>
-                    <Link className="deco sss" to={routes.login}>Login</Link>
-                    <Link className="deco" to={routes.register}>Register</Link>
-                    
-                </div>
-            </Stack>
-            </Row>
-            <Col>{children}</Col>
-        </Row>
-    
+            <CustomNav/>
+            {children}
        </React.Fragment>
     );
 };
